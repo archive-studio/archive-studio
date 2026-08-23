cd40b7ac1b01f0c809f2ca50304f58cfe816dc4c
Najib Habiburahman <rahmannajibbb@gmail.com>
2026-08-23 20:02:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ const data = {
   tshirts: {
     label: "T-SHIRTS",
     images: [
-      ["assets/tshirt-01.jpg","Endless / Find Your Self"],
-      ["assets/tshirt-02.jpg","Bad Habit / Midnight Lust"],
-      ["assets/tshirt-03.jpg","Saint"],
-      ["assets/tshirt-04.jpg","Urban Anxiety"],
-      ["assets/tshirt-05.jpg","Smoke Makes You Better Rich"]
+     ["tshirt-01.jpg","Endless / Find Your Self"],
+["tshirt-02.jpg","Bad Habit / Midnight Lust"],
+["tshirt-03.jpg","Saint"],
+["tshirt-04.jpg","Urban Anxiety"],
+["tshirt-05.jpg","Smoke Makes You Better Rich"]
     ]
   },
   pants: {
     label: "PANTS",
     images: [
-      ["assets/pants-01.jpg","Anxiety Denim Shorts"],
-      ["assets/pants-02.jpg","Red / Devotional"],
-      ["assets/pants-03.jpg","Saint Denim"],
-      ["assets/pants-04.jpg","Endless / Find Your Self"]
+      ["pants-01.jpg","Anxiety Denim Shorts"],
+["pants-02.jpg","Red / Devotional"],
+["pants-03.jpg","Saint Denim"],
+["pants-04.jpg","Endless / Find Your Self"]
     ]
   }
 };
